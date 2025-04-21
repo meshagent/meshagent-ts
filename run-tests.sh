4c2e7d357be331c95c6ce359b23e7f74e2d8fa82
Brandon Bethke <brandon.bethke@timu.com>
2025-04-21 12:42:17 -0700

    Removed unneeded meshagent-testing folder

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@ export MESHAGENT_API_URL=http://localhost:8080
 export MESHAGENT_SECRET=testsecret
 export MESHAGENT_PROJECT_ID=testproject
 export MESHAGENT_KEY_ID=testkey
-export MESHAGENT_LOCAL_SERVER_FILES_STORAGE_PATH=".local_server_documents"
+export MESHAGENT_SERVER_CLI_FILES_STORAGE_PATH=".local_server_documents"
 
 VIRTUAL_ENV=`pwd`/venv
 python3 -m venv $VIRTUAL_ENV
@@ -15,11 +15,9 @@ pip install --no-cache-dir \
             ../meshagent-tools \
             ../meshagent-openai \
             ../../meshagent-cloud \
-            ../../meshagent-testing \
             ../../meshagent-server 
 
-
-python3 ../../meshagent-testing/meshagent/testing/cli.py &
+python3 ../../meshagent-server/meshagent/server/cli/cli.py &
 CLI_PID=$!
 # When this script exits (for any reason), kill the background job
 trap 'kill $CLI_PID 2>/dev/null || true' EXIT
