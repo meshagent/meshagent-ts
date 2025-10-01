cbf2a3214aa1fcf3dd06de8628dfdbd8e41ecaa1
Brandon Bethke <brandon.bethke@timu.com>
2025-10-01 15:20:36 -0700

    updated the local test server to handle the new api keys

--- run-tests.sh
+++ run-tests.sh
@@ -11,9 +11,10 @@ done
 
 if $SETUP_SERVER; then
     export MESHAGENT_API_URL=http://localhost:8080
-    export MESHAGENT_SECRET=testsecret
-    export MESHAGENT_PROJECT_ID=testproject
-    export MESHAGENT_KEY_ID=testkey
+    export MESHAGENT_API_KEY="ma-.j.DnM2qR6WWEq4.mYmfuQ-_B55PLVWSWu_B4GCuETgWA-test-secret-secure-secret-sample2560binarykey"
+    export MESHAGENT_SECRET="test-secret-secure-secret-sample2560binarykey"
+    export MESHAGENT_PROJECT_ID="fc1e793c-b556-496b-bf07-8182b844e058"
+    export MESHAGENT_KEY_ID="fa3f839c-cdaa-47a5-9612-ae3e99899fb9"
     export MESHAGENT_SERVER_CLI_FILES_STORAGE_PATH=".local_server_documents"
 
     VIRTUAL_ENV=`pwd`/venv
